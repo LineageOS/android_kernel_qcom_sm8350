99c0ea74031e06317d7b42f09b55798662da63ee
Yue Haibing <yuehaibing@huawei.com>
2023-08-18 20:40:25 +0800

    UPSTREAM: usb: gadget: function: Remove unused declarations
    These declarations are not implemented anymore, remove them.
    
    Signed-off-by: Yue Haibing <yuehaibing@huawei.com>
    Link: https://lore.kernel.org/r/20230818124025.51576-1-yuehaibing@huawei.com
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    Bug: 301887900
    Change-Id: Ib1ac4d7ecada70d467cd9b2d688f13ba3797a05c
    (cherry picked from commit ae257611573cde279d31be3961a59e255f567fb0
     https://kernel.googlesource.com/pub/scm/linux/kernel/git/gregkh/usb usb-next)
    Signed-off-by: Avichal Rakesh <arakesh@google.com>

--- drivers/usb/gadget/function/u_serial.h
+++ drivers/usb/gadget/function/u_serial.h
@@ -61,8 +61,4 @@ void gserial_free_line(unsigned char port_line);
 int gserial_connect(struct gserial *, u8 port_num);
 void gserial_disconnect(struct gserial *);
 
-/* functions are bound to configurations by a config or gadget driver */
-int gser_bind_config(struct usb_configuration *c, u8 port_num);
-int obex_bind_config(struct usb_configuration *c, u8 port_num);
-
 #endif /* __U_SERIAL_H */

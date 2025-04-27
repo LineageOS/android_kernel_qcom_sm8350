ba2c04119eb5f7302e94e37ab6b7b20582795f44
Alexander Koskovich <akoskovich@pm.me>
2025-04-27 02:25:21 -0400

    usb: gadget: uvc: Fix compile error due to type mismatch
    Change-Id: I271c0f0160e23d8c4b9752e3f8f15eeea776a110

--- drivers/usb/gadget/function/uvc_video.c
+++ drivers/usb/gadget/function/uvc_video.c
@@ -306,7 +306,7 @@ static int uvcg_video_usb_req_queue(struct uvc_video *video,
 		 */
 		if (list_empty(&video->req_free) || ureq->last_buf ||
 			!(video->req_int_count %
-			min(DIV_ROUND_UP(video->uvc_num_requests, 4), UVCG_REQ_MAX_INT_COUNT))) {
+			min(DIV_ROUND_UP(video->uvc_num_requests, 4), (unsigned int)UVCG_REQ_MAX_INT_COUNT))) {
 			video->req_int_count = 0;
 			req->no_interrupt = 0;
 		} else {
